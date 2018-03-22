f7fe4816452854130277b0f153aa8f1f70d4f686
Kevin294 <kevin.arnesen@hotmail.com>
2018-03-22 11:18:30 +0100

    test

--- app/src/main/java/com/example/steffen/rememo/Activities/FeedActivity.java
+++ app/src/main/java/com/example/steffen/rememo/Activities/FeedActivity.java
@@ -1,7 +1,9 @@
 package com.example.steffen.rememo.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.steffen.rememo.R;
 
@@ -11,5 +13,25 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    public void onHome(View view) {
+        Intent myIntent = new Intent(view.getContext(), FeedActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void onAppealing(View view) {
+        Intent myIntent = new Intent(view.getContext(), AppealingActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void onContact(View view) {
+        Intent myIntent = new Intent(view.getContext(), ContactActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void onProfile(View view) {
+        Intent myIntent = new Intent(view.getContext(), ProfileActivity.class);
+        startActivityForResult(myIntent, 0);
     }
 }
--- app/src/main/java/com/example/steffen/rememo/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/steffen/rememo/Activities/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.example.steffen.rememo.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.steffen.rememo.R;
 
@@ -12,5 +14,24 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         //asd
+    }
+    public void onHome(View view) {
+        Intent myIntent = new Intent(view.getContext(), FeedActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void onAppealing(View view) {
+        Intent myIntent = new Intent(view.getContext(), AppealingActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void onContact(View view) {
+        Intent myIntent = new Intent(view.getContext(), ContactActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void onProfile(View view) {
+        Intent myIntent = new Intent(view.getContext(), ProfileActivity.class);
+        startActivityForResult(myIntent, 0);
     }
 }
