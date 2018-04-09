74cbb94df0e6cf6dae19b6a100d7b1f6d5992939
Kevin294 <kevin.arnesen@hotmail.com>
2018-04-09 13:51:13 +0200

    cardview implementation

--- app/src/main/java/com/example/steffen/rememo/Fragments/Fragment_Feed.java
+++ app/src/main/java/com/example/steffen/rememo/Fragments/Fragment_Feed.java
@@ -32,10 +32,10 @@ public class Fragment_Feed extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_feed, container, false);
 
         RecyclerView mRecyclerView = (RecyclerView) fragmentView.findViewById(R.id.my_recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mRecyclerView.setHasFixedSize(true);
 
         mAdapter = new MyRecyclerViewAdapter(getDataSet());
