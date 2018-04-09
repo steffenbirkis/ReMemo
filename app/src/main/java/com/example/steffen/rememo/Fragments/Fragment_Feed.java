9707291f63a3c621620b043b194682b2f959107e
Kevin294 <kevin.arnesen@hotmail.com>
2018-04-09 13:25:48 +0200

    cardview implementation

--- app/src/main/java/com/example/steffen/rememo/Fragments/Fragment_Feed.java
+++ app/src/main/java/com/example/steffen/rememo/Fragments/Fragment_Feed.java
@@ -2,21 +2,26 @@ package com.example.steffen.rememo.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
-import com.example.steffen.rememo.Logic.FirebaseLogic;
-import com.example.steffen.rememo.Logic.User;
+import com.example.steffen.rememo.Logic.DataObject;
+import com.example.steffen.rememo.Logic.MyRecyclerViewAdapter;
 import com.example.steffen.rememo.R;
+
+import java.util.ArrayList;
+
 
 public class Fragment_Feed extends Fragment {
     public static Fragment_Feed newInstance() {
         Fragment_Feed fragment = new Fragment_Feed();
         return fragment;
     }
-
+    RecyclerView.Adapter mAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +33,36 @@ public class Fragment_Feed extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
+        View fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+        RecyclerView mRecyclerView = (RecyclerView) fragmentView.findViewById(R.id.my_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager;
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
         return inflater.inflate(R.layout.fragment_feed, container, false);
 
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i("This", " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        for (int index = 0; index < 20; index++) {
+            DataObject obj = new DataObject("Some Primary Text " + index,
+                    "Secondary " + index);
+            results.add(index, obj);
+        }
+        return results;
     }
 }
