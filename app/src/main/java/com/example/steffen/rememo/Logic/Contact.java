fc50b0dba365c3f717a70ccf9bb9a7c6cc078752
steffenbirkis <birkisproductions@gmail.com>
2018-04-16 11:53:24 +0200

    Fikset kanskje profil

---
+++ app/src/main/java/com/example/steffen/rememo/Logic/Contact.java
@@ -0,0 +1,36 @@
+package com.example.steffen.rememo.Logic;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Contact {
+    private String mail;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+
+
+    public Contact(){}
+    public Contact(String mail){
+        this.mail=mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void addContact(Contact contact,User user){
+
+       String mail= FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        mDatabase=FirebaseDatabase.getInstance();
+        mRef=mDatabase.getReference().child("contacts").child(mail);
+        contact.setMail(user.getEmail());
+        mRef.setValue(contact);
+    }
+
+}
