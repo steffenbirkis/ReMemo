2978a4cb9d8a8e89ccee1da5111a71b916c6d926
Kevin294 <kevin.arnesen@hotmail.com>
2018-04-25 10:51:05 +0200

    contact heftig update

--- app/src/main/java/com/example/steffen/rememo/Activities/EditProfile.java
+++ app/src/main/java/com/example/steffen/rememo/Activities/EditProfile.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,16 +14,25 @@ import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
+import com.example.steffen.rememo.Fragments.Fragment_Contact;
+import com.example.steffen.rememo.Fragments.Fragment_Feed;
+import com.example.steffen.rememo.Logic.Contact;
 import com.example.steffen.rememo.Logic.FirebaseLogic;
 import com.example.steffen.rememo.Logic.User;
 import com.example.steffen.rememo.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -43,6 +53,9 @@ public class EditProfile extends AppCompatActivity {
     private DatabaseReference FirebaseRef;
     private ImageView mImageView;
     private Uri image;
+    private DatabaseReference mDatabase;
+    private List<Contact> cList;
+
 
 
     @Override
@@ -50,10 +63,13 @@ public class EditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_editprofile);
-
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseLogic.EncodeString(FirebaseAuth.getInstance().getCurrentUser().getEmail())).child("contacts");
+        mDatabase.addChildEventListener(contactlistener);
         mStorage = FirebaseStorage.getInstance().getReference();
         mSelectImage = (Button)findViewById(R.id.edit_picture);
         mImageView=(ImageView)findViewById(R.id.editprofile_image);
+
+        cList = new ArrayList<Contact>();
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,12 +90,19 @@ public class EditProfile extends AppCompatActivity {
                 final String tempmail= FirebaseLogic.EncodeString(mail);
 
                 FirebaseRef=fbd.getReference().child("users").child(tempmail);
-                User user=new User(name.getText().toString(),workplace.getText().toString(),mail);
+                user=new User(name.getText().toString(),workplace.getText().toString(),mail);
                 FirebaseRef.setValue(user);
                 FirebaseRef.child("background").setValue(background.getText().toString());
                 FirebaseRef.child("role").setValue(role.getText().toString());
                 FirebaseRef.child("phone").setValue(phone.getText().toString());
                 FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseLogic.EncodeString(FirebaseAuth.getInstance().getCurrentUser().getEmail())).child("photoURL").setValue(image.toString());
+
+                user.setBackground(background.getText().toString());
+                user.setRole(role.getText().toString());
+                user.setPhone(phone.getText().toString());
+                user.setPhotoURL(image.toString());
+
+                updateData();
 
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
@@ -130,8 +153,50 @@ public class EditProfile extends AppCompatActivity {
 
     }
 
+    public void updateData(){
+        DatabaseReference temp_path = FirebaseDatabase.getInstance().getReference().child("users");
+        List<String> emails = new ArrayList<String>();
+        String current = FirebaseLogic.EncodeString(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        for(Contact c:cList){
+            emails.add(FirebaseLogic.EncodeString(c.getMail()));
+        }
+        for(String s:emails){
+            temp_path.child(s).child("contacts").child(current).child("name").setValue(user.getName());
+            temp_path.child(s).child("contacts").child(current).child("workplace").setValue(user.getWorkplace());
+            temp_path.child(s).child("contacts").child(current).child("role").setValue(user.getRole());
+            temp_path.child(s).child("contacts").child(current).child("background").setValue(user.getBackground());
+            temp_path.child(s).child("contacts").child(current).child("phone").setValue(user.getPhone());
+            temp_path.child(s).child("contacts").child(current).child("photo").setValue(user.getPhotoURL());    
+
+        }
+    }
+
     public void updateImage(Uri pic){
         Glide.with(this).load(pic).into(mImageView);
     }
 
+    ChildEventListener contactlistener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot snapshot, String s) {
+
+            Contact contact = snapshot.getValue(Contact.class);
+            cList.add(contact);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            System.out.println("The read failed: " + databaseError.getCode());
+        }
+
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            return;
+        }
+
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        }
+
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        }
+
+    };
 }
